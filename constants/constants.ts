f2ebdf82b778df62fa7a810b45b26da6597aae15
pcorrado-np <pcorrado@neopower.digital>
2023-02-16 18:50:07 -0300

    tech: update contract address

--- constants/constants.ts
+++ constants/constants.ts
@@ -2,7 +2,7 @@ export const CHAINS = {
   ROCOCO: {
     NAME: "Rococo",
     URL: "wss://rococo-contracts-rpc.polkadot.io",
-    CONTRACT_ADDRESS: "5HVQQduG8T76Jdsw6o6iv38TKgkFpvtn9bag76fuGMcfPrCG",
+    CONTRACT_ADDRESS: "5CJospADQxocnDbs5kVTtwAbdvRvuwi4MmWuscesMWKsjuWR",
   },
   SHIBUYA: {
     name: "Shibuya",
